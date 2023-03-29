7e0fce569a197a81d6b00e8eefbd1d0c525f785d
DogDwarf <crazyz2000@naver.com>
2023-03-29 20:39:42 +0900

    Delete LoaManagerApi directory

--- LoaManagerApi/Models/AdminDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LoaManagerApi.Models
-{
-    public class AdminDatabaseSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string CollectionName { get; set; } = null!;
-    }
-}
